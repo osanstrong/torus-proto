72a6010c9c972d4841a193c2a91508a8b4a7cb09
Elliott Biondo <veb@ornl.gov>
2025-06-25 14:17:38 -0400

    Make test directory

--- test/test_toroid.py
+++ test/test_toroid.py
@@ -1,4 +1,14 @@
 # A script to run test cases of toroid-ray.py
+
+
+# TODO: do this in a more pythonic way
+import os
+import sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.join(current_dir, '..')
+sys.path.insert(0, parent_dir) 
+#
+
 import math
 import itertools
 import random
