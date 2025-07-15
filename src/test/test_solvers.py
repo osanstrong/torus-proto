e534afd8f629afe52c1327b53bd3b1220f06ed56
Owen Strong <owensanstrong@gmail.com>
2025-07-15 11:19:33 +0800

    Removed unused NR test method, as it is currently unused

--- src/test/test_solvers.py
+++ src/test/test_solvers.py
@@ -60,36 +60,3 @@ def test_1010_subcubics():
         droot_big = alg1010.Alg1010Solver([1,1,1,1,1])._solve_depressed_cubic_handleinf(coeffs[0], coeffs[1])
         result_big = droot_big**3 + coeffs[0]*droot_big + coeffs[1]
         assert math.isclose(result_big, 0, abs_tol=mpmath.power(2, -mpmath.mp.prec+20))
-
-# def test_1010_NR():
-#     '''Test the Newton Raphson implementation'''
-#     num_trials = 100
-#     spread = 200
-#     for i in range(num_trials):
-#         coeffs = glob_rng.normal(0, spread / 2, 4)
-
-#         roots_np = np.roots([1] + [c for c in coeffs])
-
-#         coeffs = [mpf(c) for c in coeffs]
-
-#         roots_iter = [mpc(r) for r in roots_np]
-        
-#         minroot = roots_iter[0]
-#         maxroot = roots_iter[-1]
-
-#         delta = 1.5
-
-#         miniter = minroot - delta
-#         maxiter = maxroot + delta
-
-#         roots_iter[0] = miniter
-#         roots_iter[-1] = maxiter
-
-#         # Make sure it converges upon to the original root
-#         for iter in range(10):
-#             roots_iter = alg1010.Solve1010([mpf(1)]+coeffs)._newton_raphson(coeffs, roots_iter)
-        
-#         assert math.isclose(roots_iter[0].real, minroot.real)
-#         assert math.isclose(roots_iter[-1].real, maxroot.real)
-        
-    
