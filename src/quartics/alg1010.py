53bcd2b3faf1526669f7f10465003a55a377432c
Owen Strong <owensanstrong@gmail.com>
2025-07-15 12:00:50 +0800

    Renamed J_inv to j_mat_inv to keep variable names in lowercase

--- src/quartics/alg1010.py
+++ src/quartics/alg1010.py
@@ -313,31 +313,31 @@ class Alg1010Solver:
             x02 = x[0] - x[2]
             det = x[1]*x[1] + x[1]*(-x[2]*x02 - mpf(2)*x[3]) + x[3]*(x[0]*x02 + x[3])
             if is_zero(det): break
-            J_inv: list[list[mpf|mpc]] = [[None,]*4,]*4
-            J_inv = [[0,]*4,]*4
-            J_inv = mpmath.matrix(J_inv)
-            J_inv[0,0] = x02
+            j_mat_inv: list[list[mpf|mpc]] = [[None,]*4,]*4
+            j_mat_inv = [[0,]*4,]*4
+            j_mat_inv = mpmath.matrix(j_mat_inv)
+            j_mat_inv[0,0] = x02
 
-            J_inv[0,1] = x[3] - x[1]
-            J_inv[0,2] = x[1] * x[2] - x[0] * x[3]
-            J_inv[0,3] = -x[1] * J_inv[0,1] - x[0] * J_inv[0,2]
-            J_inv[1,0] = x[0] * J_inv[0,0] + J_inv[0,1]
-            J_inv[1,1] = -x[1] * J_inv[0,0]
-            J_inv[1,2] = -x[1] * J_inv[0,1]
-            J_inv[1,3] = -x[1] * J_inv[0,2]
-            J_inv[2,0] = -J_inv[0,0]
-            J_inv[2,1] = -J_inv[0,1]
-            J_inv[2,2] = -J_inv[0,2]
-            J_inv[2,3] = J_inv[0,2] * x[2] + J_inv[0,1] * x[3]
-            J_inv[3,0] = -x[2] * J_inv[0,0] - J_inv[0,1]
-            J_inv[3,1] = J_inv[0,0] * x[3]
-            J_inv[3,2] = x[3] * J_inv[0,1]
-            J_inv[3,3] = x[3] * J_inv[0,2]
+            j_mat_inv[0,1] = x[3] - x[1]
+            j_mat_inv[0,2] = x[1] * x[2] - x[0] * x[3]
+            j_mat_inv[0,3] = -x[1] * j_mat_inv[0,1] - x[0] * j_mat_inv[0,2]
+            j_mat_inv[1,0] = x[0] * j_mat_inv[0,0] + j_mat_inv[0,1]
+            j_mat_inv[1,1] = -x[1] * j_mat_inv[0,0]
+            j_mat_inv[1,2] = -x[1] * j_mat_inv[0,1]
+            j_mat_inv[1,3] = -x[1] * j_mat_inv[0,2]
+            j_mat_inv[2,0] = -j_mat_inv[0,0]
+            j_mat_inv[2,1] = -j_mat_inv[0,1]
+            j_mat_inv[2,2] = -j_mat_inv[0,2]
+            j_mat_inv[2,3] = j_mat_inv[0,2] * x[2] + j_mat_inv[0,1] * x[3]
+            j_mat_inv[3,0] = -x[2] * j_mat_inv[0,0] - j_mat_inv[0,1]
+            j_mat_inv[3,1] = j_mat_inv[0,0] * x[3]
+            j_mat_inv[3,2] = x[3] * j_mat_inv[0,1]
+            j_mat_inv[3,3] = x[3] * j_mat_inv[0,2]
  
             dx = mpmath.matrix([0,]*4)
             for k1 in range(4):
                 for k2 in range(4):
-                    dx[k1] += J_inv[k1,k2] * fvec[k2]
+                    dx[k1] += j_mat_inv[k1,k2] * fvec[k2]
 
             x_old = [mpmathify(xi) for xi in x]
             
