de72168837938fdfb43c34a68015c2cdf308ffa7
Owen Strong <owensanstrong@gmail.com>
2025-07-30 16:23:18 -0400

    Reworked ray comparison algorithm, added more graphs (last one isn't converging)

--- src/analysis/evil_rays.py
+++ src/analysis/evil_rays.py
@@ -20,10 +20,19 @@ import src.quartics.alg1010 as alg1010
 # =-----------=
 
 
-_geab_cache = {} #Do we really need to cache EVERYTHING?
-_exp1_final = {} #Final information about experiment 1, e.g. graphs n stuff
-_dbd_cache = {}
-_exp2_final = {}
+try:
+    _geab_cache = _geab_cache #Rough check to not reload if it's already been loaded
+except NameError:
+    _geab_cache = {} #Do we really need to cache EVERYTHING?
+    _exp1_final = {} #Final information about experiment 1, e.g. graphs n stuff
+    _dbd_cache = {}
+    _exp2_final = {}
+    _dbdg_cache = {}
+    _exp2b_final = {}
+    _dbdt_cache = {}
+    _exp3_final = {}
+    _esct_cache = {}
+    _exp3b_final = {}
 
 # =--------------=
 # Useful constants
@@ -41,8 +50,10 @@ MpfAble: type = mpf|float|str
 # Experimental Presets
 # =------------------=
 
-# ITER_TOROID_CM: EllipticToroid = EllipticToroid() #Toroid the scale of ITER, though ITER is a different kind
-# ITER_OUTER_DIVERTOR_CM: EllipticToroid = EllipticToroid() #Toroid matching the outer curved section of the ITER divertor; Has more extreme difference in dimensions
+BAND_RING_CM: EllipticToroid = EllipticToroid(0.5, mpf("0.02"), 0.25) #idk, the size of finger or smt
+THIN_BAGEL_CM: EllipticToroid = EllipticToroid(2.5, 2, 0.2) #or could make it thinner and say it's a vinyl record
+ITER_TOROID_CM: EllipticToroid = EllipticToroid(6.2, 4.5, 8.75) #Toroid roughly the scale of ITER, though ITER is a different kind
+LHC_TUNNEL_CM: EllipticToroid = EllipticToroid(4.25 * 1_000 * 100, 190, 190) #LHC tunnel because yknow why not
 
 
 # =---------------=
@@ -156,7 +167,165 @@ def graph_dev_by_distance(
     plt.ylabel("Log of error (deviation, same units as distance)")
     plt.title("Error by distance for toroid 50x10x20 for normal rays approaching at different solvers and precisions")
     plt.show()
+
+
+def graph_dev_by_distance_graze(
+    num_rays: int = 10,
+    dists: list = [10**i for i in range(12)],
+    solvers: list = ["tt", "fr"],
+    precs: dict = {"single":SINGLE_PREC, "double":DOUBLE_PREC, "quad":QUAD_PREC},
+    use_cache: bool = False,
+    surf_dist: mpf = mpf(-1) #Go a little into the torus so it's supposed to hit
+):
+    tor = EllipticToroid(50,10,20)
+    dist_coords = [(d, surf_dist) for d in dists]
+    hole_radius = tor.tor_rad - tor.hor_rad
+    if not use_cache:
+        random_uvs = [(mp.rand()*2*mp.pi, (mp.rand()+1.5)*mp.pi) for i in range(num_rays)]
+        full_res = {}
+        for slv in solvers:
+            full_res[slv] = {}
+            for prec in precs:
+                res = uvs_grazes_by_distances(
+                    random_uvs, dist_coords, slv, precs[prec],
+                    tor=tor,
+                )
+                full_res[slv][prec] = res
+                _exp2b_final[f"{slv}_{prec}"] = {
+                    "mean":res["tp_mean"],
+                    "dev":res["tp_dev"],
+                    "fail":res["tp_fail"],
+                    "hp_fail":res["hp_fail"]
+                }
+                print(f"Combo {slv}_{prec} complete")
+        _dbdg_cache.update(full_res)
+        _exp2b_final["dists"] = dists
+    # If we are using cache, we just assume those are already in place
+    x = [float(log(d)) for d in dists]
+    _exp2b_final["dists"] = [float(d) for d in dists]
+    for slv in solvers:
+        for prec in precs:
+            for dataset in ["mean", "dev", "fail", "hp_fail"]: #Convert to floats for serializability
+                _exp2b_final[f"{slv}_{prec}"][dataset] = [0 if n is None else float(n) for n in _exp2b_final[f"{slv}_{prec}"][dataset]]
+            err = [float(log(n, b=10)) for n in _exp2b_final[f"{slv}_{prec}"]["dev"]]
+            plt.plot(x, err, drawstyle='steps-mid', label=f"{slv} at {prec}")
+    plt.legend(title="Solver-precision combo:")
+    plt.xlabel("Log of distance")
+    plt.ylabel("Log of error (deviation, same units as distance)")
+    plt.title("Error by distance for toroid 50x10x20 for grazing rays approaching at different solvers and precisions")
+    plt.show()
     
+
+def graph_dbd_by_toroid(
+    num_rays: int = 10,
+    toroids: dict = {
+        "ring":BAND_RING_CM,
+        "disc":THIN_BAGEL_CM,
+        "iter":ITER_TOROID_CM,
+        "lhc":LHC_TUNNEL_CM,
+    },
+    dists: list = [10**i for i in range(-10,20,2)],
+    solver: str = "tt",
+    prec: int = DOUBLE_PREC,
+    use_cache: bool = False,
+):
+    if not use_cache:
+        random_uvs = [(mp.rand()*2*mp.pi, (mp.rand()+1.5)*mp.pi) for i in range(num_rays)]
+        full_res = {}
+        for tor_name in toroids:
+            tor = toroids[tor_name]
+            res = uvs_normals_by_distances(random_uvs, tor=tor, dists=dists, prec=prec, face_outwards=True, solver_code=solver)
+            full_res[tor_name] = res
+            _exp3_final[tor_name] = {
+                "mean":res["tp_mean"],
+                "dev":res["tp_dev"],
+                "fail":res["tp_fail"],
+                "hp_fail":res["hp_fail"]
+            }
+            print(" "*40, end="\r")
+            print(f"Toroid {tor_name} complete")
+        _dbdg_cache.update(full_res)
+        _exp3_final["dists"] = dists
+    # If we are using cache, we just assume those are already in place
+    x = [float(log(d)) for d in dists]
+    _exp3_final["dists"] = [float(d) for d in dists]
+    for tor_name in toroids:
+        for dataset in ["mean", "dev", "fail", "hp_fail"]: #Convert to floats for serializability
+            _exp3_final[tor_name][dataset] = [0 if n is None else float(n) for n in _exp3_final[tor_name][dataset]]
+        err = [float(log(n, b=10)) for n in _exp3_final[tor_name]["dev"]]
+        plt.plot(x, err, drawstyle='steps-mid', label=f"{tor_name}:{toroids[tor_name]}")
+    plt.legend(title="Toroid:")
+    plt.xlabel("Log of distance")
+    plt.ylabel("Log of error (deviation, same units as distance)")
+    plt.title(f"Error by distance for toroids for grazing rays approaching at precision {prec}")
+    plt.show()
+
+
+def graph_escape_by_toroid_random_ranges(
+    num_rays: int = 10,
+    toroids: dict = {
+        "ring":BAND_RING_CM,
+        "disc":THIN_BAGEL_CM,
+        "iter":ITER_TOROID_CM,
+        "lhc":LHC_TUNNEL_CM,
+    },
+    uv_ranges: dict = {
+        "outside":[0, 2*mp.pi, -0.1, 1],
+        "inside":[0, 2*mp.pi, mp.pi-0.1, mp.pi+0.1],
+        "top":[0, 2*mp.pi, 0.5*mp.pi-0.1, 0.5*mp.pi+0.1],
+        "bottom":[0, 2*mp.pi, 1.5*mp.pi-0.1, 1.5*mp.pi+0.1]
+    },
+    start_dists: list = [power(10,i) for i in [-300,-40,-0.01, 10]],
+    solver: str = "tt",
+    prec: int = DOUBLE_PREC,
+    use_cache: bool = False,
+):
+    if not use_cache:
+        uv_sets = {}
+        for range_name in uv_ranges:
+            uvr = uv_ranges[range_name]
+            random_uvs = [(uvr[0] + (uvr[1]-uvr[0])*mp.rand(), uvr[2] + (uvr[3]-uvr[2])*mp.rand()) for i in range(num_rays)]
+            uv_sets[range_name] = random_uvs
+
+        _esct_cache["rays"] = uv_sets
+        for tor_name in toroids:
+            tor = toroids[tor_name]
+            for range_name in uv_ranges:
+                res = epsilon_avoid_backcollision(epsilons=start_dists, uv_pairs=uv_sets[range_name], prec=prec, solver_code=solver)
+                _esct_cache[f"{tor_name}_{range_name}"] = res
+                last_fail_idx = max([i if not res["valid"][i] else -99999 for i in range(len(res["valid"]))])
+                closest = res["dists"][last_fail_idx+1]
+                _exp3b_final[f"{tor_name}_{range_name}"] = closest
+
+    range_names = [rn for rn in uv_ranges]
+    tor_names = [tn for tn in toroids]
+    x = arange(len(range_names)) #Label locations
+    width = 0.25 #Width of bars
+
+    for tor_name in tor_names:
+        for range_name in range_names: #Convert to floats for serializability
+            _exp3b_final[f"{tor_name}_{range_name}"] = float(_exp3b_final[f"{tor_name}_{range_name}"])
+
+    fig, ax = plt.subplots(layout='constrained')
+    for i in range(len(tor_names)):
+        # prec = precs[i]
+        tor_name = tor_names[i]
+        offset = width*i
+        pos = x+offset
+        rects = ax.bar(pos, [float(-log(_exp3b_final[f"{tor_name}_{range_names[j]}"], b=10)) for j in len(range_names)], width, label=f"{tor_name}:{toroids[tor_name]}")
+        # print(rects[:])
+        print(f"location: {pos}, data: {[_exp3b_final[f"{tor_name}_{range_names[j]}"] for j in len(range_names)]}, width: {width}")
+        ax.bar_label(rects, padding=3)
+
+    # Add text
+    ax.set_ylabel("Closest escape distance d, -log(d)")
+    ax.set_title(f"Closest safe distances from assorted toroids, for {num_rays} rays each in distinct regions")
+    ax.set_xticks(x+width, precs)
+    ax.legend(loc="upper left", ncols=3)
+    plt.show()
+
+
+
 
 
 
@@ -185,7 +354,7 @@ def epsilon_avoid_backcollision(
     log_convergence: MpfAble = "0.1",
     polyn_calc_prec: int = None,
     solver_code: str = "tt",
-    verbose: bool = False,
+    verbosity: int = 1,
 ) -> dict:
     '''
     How close can a ray start to the surface of a toroid and reliably
@@ -214,7 +383,7 @@ def epsilon_avoid_backcollision(
     dist_results = lambda dists: uvs_normals_by_distances(
         uv_pairs, dists=dists, face_outwards=True, 
         prec=prec, polyn_calc_prec=polyn_calc_prec,
-        verbose=verbose,
+        verbosity=verbosity,
         solver_code=solver_code
     )
     full_results = dist_results(epsilons)
@@ -224,13 +393,19 @@ def epsilon_avoid_backcollision(
     after_hf = dist_results([full_results["dists"][highest_fail]*power(10, log_convergence)])
     
     def insert_results(new_res, idx, full_res, pf_res):
-        new_raws = new_res["raw_results"]
-        new_res["raw_results"] = []
-        # print(to_df(new_res))
-        full_raws = full_res["raw_results"]
-        for i in range(len(full_raws)):
-            full_raws[i] = insert_dict_at_index(full_raws[i], new_raws[i], idx)
-        
+        # for key in new_res: print(key)
+        # possible_logkeys = ["full_logs", "raw_results"]
+        # logkey = "full_logs"
+        # # for key in possible_logkeys:
+        # #     if key in new_res:
+        # #         logkey = key
+        # new_raws = new_res[logkey]
+        # new_res[logkey] = []
+        # # print(to_df(new_res))
+        # full_raws = full_res[logkey]
+        # for i in range(len(full_raws)):
+        #     full_raws[i] = insert_dict_at_index(full_raws[i], new_raws[i], idx)
+
         insert_dict_at_index(full_res, new_res, idx)
         pf_res[idx:idx] = [new_res["tp_mean"][0] is None]
 
@@ -240,9 +415,12 @@ def epsilon_avoid_backcollision(
     max_iter = 100
     c = 0
     while (not passfail_history[highest_fail+1]) and (c < max_iter):
-        print(f"Iteration {c} complete, further iteration needed.")
         highest_dist = full_results["dists"][highest_fail+1]
-        next_dist = full_results["dists"][highest_fail+2]
+        print(f"Iteration {c} complete after dist {highest_dist}, further iteration needed.")
+        if (highest_fail+2 >= len(full_results["dists"])): #biggest, go a little bigger
+            next_dist = full_results["dists"][highest_fail+1]*power(10, log_convergence)
+        else:
+            next_dist = full_results["dists"][highest_fail+2]
         betw_dist = mp.sqrt(highest_dist*next_dist)
         betw_result = dist_results([betw_dist])
         betw_passfail = betw_result["tp_mean"][0] is None
@@ -448,6 +626,26 @@ def uvrand_normals(
     return uvs_normals_by_distances(uvs, tor=tor, dists=dists,prec=prec)
 
 
+def uvs_grazes_by_distances(
+    uvs: list[tuple],
+    dists: list[tuple[MpfAble]], # Ray distances, then normal distances
+    solver_code: str,
+    prec: int,
+    tor: EllipticToroid = EllipticToroid(50,10,20),
+    verbosity: int = 1,
+    fail_condition: callable = None
+) -> dict:
+    '''
+    Compares the results of different distance pairs (along ray and away from surface)
+    on a set of uvs, against their high-precision counterparts
+    '''
+    hole_radius = tor.tor_rad - tor.hor_rad
+    if fail_condition is None: fail_condition = lambda dist, set_idx, ray_idx: dist is None or abs(dist-dists[set_idx][0]) > hole_radius # Any further out and we assume it might be (correctly) detecting the intersection on the other side
+    raysets = [[rg.get_grazing_ray(tor, u=uv[0], v=uv[1], distance=d[0], pos_epsilon=d[1]) for uv in uvs] for d in dists]
+    return compare_raysets_vs_hp(raysets, solver_code, prec, toroid=tor, is_failure=fail_condition, verbosity=verbosity)
+    
+
+
 def uvs_normals_by_distances(
     uvs,
     tor: EllipticToroid = EllipticToroid(50, 10, 20),
@@ -456,10 +654,14 @@ def uvs_normals_by_distances(
     face_outwards: bool = False,
     polyn_calc_prec: int = None,
     solver_code: str = "tt",
-    verbose: bool = False,
-    silent: bool = False,
+    verbosity: int = 1
 ) -> dict:
     result_list = []
+
+    raysets = [[rg.get_normal_ray(tor, uv[0], uv[1]*(-1 if face_outwards else 1), d) for uv in uvs] for d in dists]
+    full_res = compare_raysets_vs_hp(raysets, solver_code, prec, tor, verbosity=verbosity)
+    full_res["dists"] = dists
+    return full_res
     
     for uv in uvs:
         u, v = uv
@@ -489,6 +691,7 @@ def uvs_normals_by_distances(
     
         tp_failcount = 0 # Includes hp_failcount for these two
         hp_failcount = 0
+        both_succeedcount = 0
         for r in result_list:
             tp_result = r[solver_code][i]
             hp_result = r[f"{solver_code}_hp"][i]
@@ -500,12 +703,13 @@ def uvs_normals_by_distances(
             else:
                 tp_mean_sum += tp_result
                 if not hp_result is None:
+                    both_succeedcount += 1
                     tp_dev_sum += (tp_result-hp_result)**2    
         
         uv_count = len(result_list)
         tp_successes = mpf(uv_count - tp_failcount)
         tp_mean = None if tp_successes == 0 else tp_mean_sum / tp_successes
-        tp_dev = None if tp_successes-hp_failcount == 0 else mp.sqrt(tp_dev_sum / (tp_successes-hp_failcount))
+        tp_dev = None if tp_successes-hp_failcount == 0 else mp.sqrt(tp_dev_sum / mpf(both_succeedcount))
         tp_devs.append(tp_dev)
         tp_means.append(tp_mean)
         tp_failrates.append(mpf(tp_failcount)/mpf(uv_count))
@@ -532,6 +736,91 @@ def get_first_intersection_z(tor, ray_src, ray_dir, slv):
         return inters[0][2]
     else:
         return None
+
+
+def compare_raysets_vs_hp(
+    raysets: Iterable[Iterable[tuple[matrix, matrix]]],
+    solver_name: str,
+    target_prec: int,
+    toroid: EllipticToroid = EllipticToroid(50,10,20),
+    high_prec: int = HIGH_PREC,
+    polyn_calc_prec: int = None, 
+    is_failure: callable = lambda dist, set_idx, ray_idx: dist is None,
+    verbosity: int = 0
+) -> dict:
+    '''
+    Parameters
+    ----------
+    raysets : Iterable[Iterable[tuple[matrix src, matrix dir] ray] rayset]
+        Sets of comparable rays; The results will be averaged over each set (inner iterable)
+        and returned as lists
+    is_failure : callable(dist, set_idx, ray_idx)
+        For the given ray in the given rayset, what defines if it were to fail?
+        By default, just if it didn't find an intersection, but this could be adjusted to 
+        exclude intersections that would belong to other parts of the toroid, for example
+    '''
+    final_results = {
+        "full_logs":[],
+        "tp_mean":[],
+        "tp_dev":[],
+        "tp_fail":[],
+        "hp_fail":[]
+    }
+    solver = get_solver(solver_name)
+    for set_idx in range(len(raysets)):
+        rayset = raysets[set_idx]
+        mean_sum = 0
+        dev_sum = 0
+        tp_failcount = 0
+        hp_failcount = 0
+        both_succeedcount = 0
+
+        num_rays = len(rayset)
+        logs = []
+        c = 0
+        for ray_idx in range(num_rays):
+            ray = rayset[ray_idx]
+            tracer = AlgTracer(solver)
+            tracer.begin()
+            mp.prec = target_prec
+            tp_dtb = toroid.distance_to_boundary(ray[0], ray[1], solver)
+            mp.prec = high_prec
+            hp_dtb = toroid.distance_to_boundary(ray[0], ray[1], solver)
+            tracer.end()
+            logs.append(tracer.simple_logstring())
+
+            tp_fail = is_failure(tp_dtb, set_idx, ray_idx)
+            hp_fail = is_failure(hp_dtb, set_idx, ray_idx)
+
+            if tp_fail: tp_failcount+=1
+            if hp_fail: hp_failcount+=1
+
+            # Average: add if it succeeds
+            if not tp_fail:
+                mean_sum += tp_dtb
+                # Deviation: add if there's also a hp to compare to
+                if not hp_fail:
+                    both_succeedcount += 1
+                    dev_sum += (tp_dtb-hp_dtb)**2
+            
+            c += 1
+            if verbosity >= 0: 
+                print(" "*40, end="\r")
+                print(f"Set {set_idx} rays completed: {c}", end='\r')
+
+        
+        mean = None if (num_rays-tp_failcount)==0 else mean_sum / mpf(num_rays - tp_failcount)
+        dev = None if both_succeedcount==0 else mp.sqrt(dev_sum / mpf(both_succeedcount))
+        tp_failrate = mpf(tp_failcount) / mpf(num_rays)
+        hp_failrate = mpf(hp_failcount) / mpf(num_rays)
+
+        final_results["full_logs"].append(logs)
+        final_results["tp_mean"].append(mean)
+        final_results["tp_dev"].append(dev)
+        final_results["tp_fail"].append(tp_failrate)
+        final_results["hp_fail"].append(hp_failrate)
+    return final_results
+
 
 
 def compare_intersections(
@@ -724,9 +1013,9 @@ def concat_dicts(dict1, dict2) -> dict:
 def insert_dict_at_index(base_dict, insert_dict, idx) -> dict:
     if not len(base_dict) == len(insert_dict) or \
         not all(key1 in insert_dict for key1 in base_dict):
-        raise ValueError(f"Dictionaries must have identical keys. d1: {base_dict}, d2: {insert_dict}")
+        raise ValueError(f"Dictionaries must have identical keys. d1: {[key for key in base_dict]}, d2: {[key for key in insert_dict]}")
     if not all(isinstance(base_dict[k], list) and isinstance(insert_dict[k], list) for k in base_dict):
-        raise ValueError(f"Dictionaries must have list data to concatenate with each other. d1: {base_dict}, d2: {insert_dict}")
+        raise ValueError(f"Dictionaries must have list data to concatenate with each other. d1: {[key for key in base_dict]}, d2: {[key for key in insert_dict]}")
 
     # new_dict = {}
     for key in base_dict:
